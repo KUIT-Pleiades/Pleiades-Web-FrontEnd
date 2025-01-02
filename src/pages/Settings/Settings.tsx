96ee97761fcd4c39a82d946b670ea90f06ec66e2
FrogUni <80472206+danpung628@users.noreply.github.com>
2025-01-02 13:31:40 +0900

    Fix: "오류 수정"
    Settings -> settings 변경 완료
    
    scss import : set -> s 변경 완료

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import set from "./Settings.module.scss";
+import s from "./settings.module.scss";
 import charater3 from "../../assets/character3.svg";
 import character1body from "../../assets/character1body.svg";
 import resetBtn from "../../assets/resetBtn.svg";
@@ -8,42 +8,42 @@ const Settings = () => {
 	const [activeMenu, setActiveMenu] = useState("face");
   return (
     <>
-      <div className={set.showCharacter}>
-        <p className={set.pHeader}>캐릭터 꾸미기</p>
-        <button className={set.nextBtn}>다음</button>
-        <p className={set.pDescription}>내 캐릭터는 어떤 모습인가요?</p>
-        <img className={set.characterFace} src={charater3} alt="" />
-        <img className={set.characterBody} src={character1body} alt="" />
-        <img className={set.resetBtn} src={resetBtn} alt="" />
+      <div className={s.showCharacter}>
+        <p className={s.pHeader}>캐릭터 꾸미기</p>
+        <button className={s.nextBtn}>다음</button>
+        <p className={s.pDescription}>내 캐릭터는 어떤 모습인가요?</p>
+        <img className={s.characterFace} src={charater3} alt="" />
+        <img className={s.characterBody} src={character1body} alt="" />
+        <img className={s.resetBtn} src={resetBtn} alt="" />
       </div>
-      <div className={set.setCharacter}>
-        <div className={set.menuBar}>
+      <div className={s.setCharacter}>
+        <div className={s.menuBar}>
           <button
-            className={`${set.menuItem} ${
-              activeMenu === "face" ? set.active : set.inactive
+            className={`${s.menuItem} ${
+              activeMenu === "face" ? s.active : s.inactive
             }`}
             onClick={() => setActiveMenu("face")}
           >
             얼굴
           </button>
           <button
-            className={`${set.menuItem} ${
-              activeMenu === "costume" ? set.active : set.inactive
+            className={`${s.menuItem} ${
+              activeMenu === "costume" ? s.active : s.inactive
             }`}
             onClick={() => setActiveMenu("costume")}
           >
             의상
           </button>
           <button
-            className={`${set.menuItem} ${
-              activeMenu === "accessory" ? set.active : set.inactive
+            className={`${s.menuItem} ${
+              activeMenu === "accessory" ? s.active : s.inactive
             }`}
             onClick={() => setActiveMenu("accessory")}
           >
             악세사리
           </button>
         </div>
-        <div className={set.contentArea}>
+        <div className={s.contentArea}>
           {activeMenu === "face" && <div>얼굴 선택 영역</div>}
           {activeMenu === "costume" && <div>의상 선택 영역</div>}
           {activeMenu === "accessory" && <div>악세사리 선택 영역</div>}
