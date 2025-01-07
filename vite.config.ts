60385564166ddab03a30fb765b6a8fe476cffa44
KANG-YEONWOOK <wook1833@gmail.com>
2025-01-07 22:57:21 +0900

    Fix: build Error fix3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx"], // 파일 확장자를 명시적으로 설정
+  },
+});
