24d08ed70de96aa994c0233405da816b26a46f6d
KANG-YEONWOOK <wook1833@gmail.com>
2025-01-07 22:54:59 +0900

    Fix: Build Error Fix2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home/Home";
 import Rocket from "./pages/Rocket/Rocket";
 import Settings from "./pages/Settings/Settings";
 import Station from "./pages/Station/Station";
-import LogIn from "./pages/LogInPage/LogInPage.tsx";
+import LogIn from "./pages/LogInPage/LogInPage";
 
 export default function App() {
   return (
