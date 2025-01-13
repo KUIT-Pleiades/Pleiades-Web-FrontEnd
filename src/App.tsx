9071de5d9021bc4f66b3abc9ec64ee12d85ff4ab
KANG-YEONWOOK <wook1833@gmail.com>
2025-01-13 20:28:47 +0900

    Feat: Add NaverLogin Router

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Rocket from "./pages/Rocket/Rocket";
 import Settings from "./pages/Settings/Settings";
 import Station from "./pages/Station/Station";
 import LogIn from "./pages/LogIn/LogIn";
+import NaverLogin from "./pages/LogIn/NaverLogin";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Splash />} />
         <Route path="/login" element={<LogIn />}></Route>
+        <Route path="naverlogin" element={<NaverLogin />}></Route>
         <Route path="home" element={<Home />}></Route>
         <Route path="rocket" element={<Rocket />}></Route>
         <Route path="settings" element={<Settings />}></Route>
