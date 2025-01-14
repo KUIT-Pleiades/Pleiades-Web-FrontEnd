6255dd3b95b6124757c2476bd7757e1846ec5546
FrogUni <80472206+danpung628@users.noreply.github.com>
2025-01-14 23:28:29 +0900

    Fix: adjust grid Items position

--- src/assets/ImageData/FaceImage.tsx
+++ src/assets/ImageData/FaceImage.tsx
@@ -8,9 +8,13 @@ import skin07 from "../Character/face/skin/skin07.png";
 
 import hair01 from "../Character/face/hair/hair01.png";
 import hair02 from "../Character/face/hair/hair02.png";
+import hair03 from "../Character/face/hair/hair03.png";
+import hair04 from "../Character/face/hair/hair04.png";
 
 import face01 from "../Character/face/face/face01.png";
 import face02 from "../Character/face/face/face02.png";
+import face03 from "../Character/face/face/face03.png";
+import face04 from "../Character/face/face/face04.png";
 
 export interface FaceItem {
   id: number;
@@ -18,63 +22,46 @@ export interface FaceItem {
   tags: string;
 }
 
+const images = {
+  skin: {
+    skin01,
+    skin02,
+    skin03,
+    skin04,
+    skin05,
+    skin06,
+    skin07,
+  },
+  hair: {
+    hair01,
+    hair02,
+    hair03,
+    hair04,
+  },
+  face: {
+    face01,
+    face02,
+    face03,
+    face04,
+  },
+};
+// FaceImages 배열 생성
 export const FaceImages: FaceItem[] = [
-  {
-    id: 1,
-    src: skin01,
-    tags: "피부"
-  },
-  {
-    id: 2,
-    src: skin02,
-    tags: "피부"
-  },
-  {
-    id: 3,
-    src: skin03,
-    tags: "피부"
-  },
-  {
-    id: 4,
-    src: skin04,
-    tags: "피부"
-  },
-  {
-    id: 5,
-    src: skin05,
-    tags: "피부"
-  },
-  {
-    id: 6,
-    src: skin06,
-    tags: "피부"
-  },
-  {
-    id: 7,
-    src: skin07,
-    tags: "피부"
-  },
-  {
-    id: 8,
-    src: hair01,
-    tags: "머리"
-  },
-  {
-    id: 9,
-    src: hair02,
-    tags: "머리"
-  },
-  {
-    id: 10,
-    src: face01,
-    tags: "표정"
-  },
-  {
-    id: 11,
-    src: face02,
-    tags: "표정"
-  }
-]
-
+  ...Object.values(images.skin).map((src, index) => ({
+    id: index + 1,
+    src,
+    tags: "피부",
+  })),
+  ...Object.values(images.hair).map((src, index) => ({
+    id: index + 8,
+    src,
+    tags: "머리",
+  })),
+  ...Object.values(images.face).map((src, index) => ({
+    id: index + 12,
+    src,
+    tags: "표정",
+  })),
+];
 
 export const faceCategories = ["전체", "피부색", "머리", "표정"];
