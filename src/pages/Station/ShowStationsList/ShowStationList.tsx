e686da3c731a3112fa77bb5e8648b259799bd2b8
hyungyu-02 <yona20020209@gmail.com>
2025-01-24 22:49:17 +0900

    Feat: StationList page Head Container

--- src/pages/Station/ShowStationsList/ShowStationList.tsx
+++ src/pages/Station/ShowStationsList/ShowStationList.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import s from './ShowStationList.module.scss';
+
+import searchIcon from '../../../assets/StationList/searchIcon.svg';
+import createIcon from '../../../assets/StationList/createIcon.svg';
+import { Character, Stations } from '../../../interfaces/Interfaces';
 
 const ShowStationList: React.FC = () => {
+  const [character, setCharacter] = useState<Character | null>(null);
+  const [stations, setStations] = useState<Stations | null>(null);
+
+  useEffect(() => {
+    fetch("/src/mock/character1.json")
+      .then((res) => res.json())
+      .then((data) => {setCharacter(data)})
+      .catch((err) => {console.error(err)});
+    fetch("/src/mock/myStations.json")
+      .then((res) => res.json())
+      .then((data) => {setStations(data)})
+      .catch((err) => {console.error(err)});
+  }, []);
+
+      
   return (
-    <div>
-      정거장 목록 구현할 위치~~
+    <div className={s.container}>
+      <div className={s.headContainer}>
+        <div className={s.title}>
+          <div className={s.titleText}>{character?.characterName}님 어디로 떠나볼까요?</div>
+          <div className={s.titleButtons}>
+            <button className={s.searchStationButton}>
+              <img src={searchIcon} alt="searchIcon" />
+            </button>
+            <button className={s.createStationButton}>
+              <img src={createIcon} alt="createIcon" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className={s.separator}>
+        <div className={s.totalNumOfStations}>
+          전체 {stations?.stations?.length || 0}
+        </div>
+      </div>
     </div>
   )
 }
