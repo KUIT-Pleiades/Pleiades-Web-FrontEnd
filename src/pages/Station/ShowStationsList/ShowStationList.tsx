2b75b634cfdda7bbd13debb9b265ff683915edc5
hyungyu-02 <yona20020209@gmail.com>
2025-01-26 17:48:17 +0900

    Feat: ShowStationList page without search popup

--- src/pages/Station/ShowStationsList/ShowStationList.tsx
+++ src/pages/Station/ShowStationsList/ShowStationList.tsx
@@ -4,10 +4,13 @@ import s from './ShowStationList.module.scss';
 import searchIcon from '../../../assets/StationList/searchIcon.svg';
 import createIcon from '../../../assets/StationList/createIcon.svg';
 import { Character, Stations } from '../../../interfaces/Interfaces';
+import SortCriteriaBox from '../../../components/SortCriteriaBox/SortCriteriaBox';
+import StationDisplay from './StationDisplay/StationDisplay';
 
 const ShowStationList: React.FC = () => {
   const [character, setCharacter] = useState<Character | null>(null);
   const [stations, setStations] = useState<Stations | null>(null);
+  const [sortCriteria, setSortCriteria] = useState<"최신순" | "이름순">("최신순");
 
   useEffect(() => {
     fetch("/src/mock/character1.json")
@@ -20,6 +23,18 @@ const ShowStationList: React.FC = () => {
       .catch((err) => {console.error(err)});
   }, []);
 
+  const sortedStations = React.useMemo(() => {
+    if (!stations) return null;
+
+    const sorted = [...stations.stations];
+    if (sortCriteria === "최신순") {
+      sorted.sort((a, b) => b.createdAt - a.createdAt); // 타임스탬프 기준 내림차순 정렬
+    } else if (sortCriteria === "이름순") {
+      sorted.sort((a, b) => a.name.localeCompare(b.name, "ko")); // 이름순 오름차순 정렬
+    }
+
+    return sorted;
+  }, [stations, sortCriteria]);
       
   return (
     <div className={s.container}>
@@ -41,7 +56,28 @@ const ShowStationList: React.FC = () => {
         <div className={s.totalNumOfStations}>
           전체 {stations?.stations?.length || 0}
         </div>
+        <div className={s.sortCriteriaBoxContainer}>
+          <SortCriteriaBox
+            sortCriteria={sortCriteria}
+            setSortCriteria={setSortCriteria}
+            textColor="var(--gray-7, #E1E1E1)"
+          />
+        </div>
       </div>
+      <div className={s.stationListWrapper}>
+        <div className={s.stationListContainer}>
+          {sortedStations?.map((station) => (
+            <div key={station.id} className={s.stationDisplayWrapper}>
+              <StationDisplay
+                name={station.name}
+                numOfUsers={station.numOfUsers}
+              />
+            </div>
+            
+          ))}
+        </div>
+      </div>
+      
     </div>
   )
 }
