7c59fb5645dbe2c3fa8caa6ee077b2013cb6e0a8
FrogUni <80472206+danpung628@users.noreply.github.com>
2025-01-26 14:13:02 +0900

    Fix: change overflow to div

--- src/pages/Settings/BackgroundSetUp.tsx
+++ src/pages/Settings/BackgroundSetUp.tsx
@@ -1,7 +1,7 @@
 import s from "./backgroundSetUp.module.scss";
 import { useCharacterStore } from "../../store/useCharacterStore";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import BackgroundTab from "./BackgroundTab";
 
 interface BackgroundSetUpProps {
@@ -18,18 +18,11 @@ const BackgroundSetUp = ({
 
   const backgroundStyle = {
     backgroundImage: `url(${character.background.imgurl})`,
+    overflow: "hidden",
   };
 
   const [showList, setShowList] = useState(true);
 
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    
-    // 컴포넌트가 언마운트될 때 원래 상태로 복구
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, []);
 
 
   return (
