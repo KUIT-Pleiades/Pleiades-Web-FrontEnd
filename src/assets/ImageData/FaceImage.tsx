e788f10bdc7b48988bcd6f3a36cb6b869eda9a0e
FrogUni <80472206+danpung628@users.noreply.github.com>
2025-02-03 19:45:36 +0900

    FIx: change FaceImage data

--- src/assets/ImageData/FaceImage.tsx
+++ src/assets/ImageData/FaceImage.tsx
@@ -1,18 +1,12 @@
-//import skin01 from "../Character/face/skin/skin01.png";
-const skin_01 =
-  "https://gateway.pinata.cloud/ipfs/bafybeigoq2wasnxjcmt7ricrmrp6jju5mui5rcigqj4o3sv6k23ch7rzhi/skin_01.png";
-const skin_02 =
-  "https://gateway.pinata.cloud/ipfs/bafybeigoq2wasnxjcmt7ricrmrp6jju5mui5rcigqj4o3sv6k23ch7rzhi/skin_02.png";
-const skin_03 =
-  "https://gateway.pinata.cloud/ipfs/bafybeigoq2wasnxjcmt7ricrmrp6jju5mui5rcigqj4o3sv6k23ch7rzhi/skin_03.png";
-const skin_04 =
-  "https://gateway.pinata.cloud/ipfs/bafybeigoq2wasnxjcmt7ricrmrp6jju5mui5rcigqj4o3sv6k23ch7rzhi/skin_04.png";
-const skin_05 =
-  "https://gateway.pinata.cloud/ipfs/bafybeigoq2wasnxjcmt7ricrmrp6jju5mui5rcigqj4o3sv6k23ch7rzhi/skin_05.png";
-const skin_06 =
-  "https://gateway.pinata.cloud/ipfs/bafybeigoq2wasnxjcmt7ricrmrp6jju5mui5rcigqj4o3sv6k23ch7rzhi/skin_06.png";
-const skin_07 =
-  "https://gateway.pinata.cloud/ipfs/bafybeigoq2wasnxjcmt7ricrmrp6jju5mui5rcigqj4o3sv6k23ch7rzhi/skin_07.png";
+const IMG_BASE_URL: string = import.meta.env.VITE_PINATA_ENDPOINT;
+
+const skin_01 = `${IMG_BASE_URL}skin_01.png`;
+const skin_02 = `${IMG_BASE_URL}skin_02.png`;
+const skin_03 = `${IMG_BASE_URL}skin_03.png`;
+const skin_04 = `${IMG_BASE_URL}skin_04.png`;
+const skin_05 = `${IMG_BASE_URL}skin_05.png`;
+const skin_06 = `${IMG_BASE_URL}skin_06.png`;
+const skin_07 = `${IMG_BASE_URL}skin_07.png`;
 // import skin02 from "../Character/face/skin/skin02.png";
 // import skin03 from "../Character/face/skin/skin03.png";
 // import skin04 from "../Character/face/skin/skin04.png";
@@ -21,26 +15,27 @@ const skin_07 =
 // import skin07 from "../Character/face/skin/skin07.png";
 
 //import hair01 from "../Character/face/hair/hair01.png";
-const hair_01 =
-  "https://gateway.pinata.cloud/ipfs/bafybeigoq2wasnxjcmt7ricrmrp6jju5mui5rcigqj4o3sv6k23ch7rzhi/hair_01.png";
-import hair_02 from "../Character/face/hair/hair02.png";
-import hair_03 from "../Character/face/hair/hair03.png";
-import hair_04 from "../Character/face/hair/hair04.png";
-import hair_05 from "../Character/face/hair/hair05.png";
-import hair_06 from "../Character/face/hair/hair06.png";
-import hair_07 from "../Character/face/hair/hair07.png";
-import hair_08 from "../Character/face/hair/hair08.png";
-import hair_09 from "../Character/face/hair/hair09.png";
+const hair_01 = `${IMG_BASE_URL}hair_01.png`;
+const hair_02 = `${IMG_BASE_URL}hair_02.png`;
+const hair_03 = `${IMG_BASE_URL}hair_03.png`;
+const hair_04 = `${IMG_BASE_URL}hair_04.png`;
+const hair_05 = `${IMG_BASE_URL}hair_05.png`;
+const hair_06 = `${IMG_BASE_URL}hair_06.png`;
+const hair_07 = `${IMG_BASE_URL}hair_07.png`;
+const hair_08 = `${IMG_BASE_URL}hair_08.png`;
+const hair_09 = `${IMG_BASE_URL}hair_09.png`;
 
-import face_01 from "../Character/face/face/face01.png";
-import face_02 from "../Character/face/face/face02.png";
-import face_03 from "../Character/face/face/face03.png";
-import face_04 from "../Character/face/face/face04.png";
-import face_05 from "../Character/face/face/face05.png";
-import face_06 from "../Character/face/face/face06.png";
-import face_07 from "../Character/face/face/face07.png";
-import face_08 from "../Character/face/face/face08.png";
-import face_09 from "../Character/face/face/face09.png";
+
+const face_01 = `${IMG_BASE_URL}face_01.png`;
+const face_02 = `${IMG_BASE_URL}face_02.png`;
+const face_03 = `${IMG_BASE_URL}face_03.png`;
+const face_04 = `${IMG_BASE_URL}face_04.png`;
+const face_05 = `${IMG_BASE_URL}face_05.png`;
+const face_06 = `${IMG_BASE_URL}face_06.png`;
+const face_07 = `${IMG_BASE_URL}face_07.png`;
+const face_08 = `${IMG_BASE_URL}face_08.png`;
+const face_09 = `${IMG_BASE_URL}face_09.png`;
+
 
 export interface FaceItem {
   name: string;
