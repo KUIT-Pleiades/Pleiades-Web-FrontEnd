d3d0512d74d958b56f4a8a345e31c4270a087a0e
hyungyu-02 <yona20020209@gmail.com>
2025-02-14 20:24:36 +0900

    Feat: stationlist page design update complete

--- src/pages/Station/ShowStationsList/ShowStationList.tsx
+++ src/pages/Station/ShowStationsList/ShowStationList.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import s from './ShowStationList.module.scss';
 import { useNavigate } from 'react-router-dom';
 
-import characterData from '../../../mock/character1.json';
+//import characterData from '../../../mock/character1.json';
 import stationsData from '../../../mock/myStations.json';
-
-import searchIcon from '../../../assets/StationList/searchIcon.svg';
-import createIcon from '../../../assets/StationList/createIcon.svg';
-import { Character, Stations } from '../../../interfaces/Interfaces';
+import { Stations } from '../../../interfaces/Interfaces';
 import SortCriteriaBox from '../../../components/SortCriteriaBox/SortCriteriaBox';
 import StationDisplay from './StationDisplay/StationDisplay';
 import SearchStationModal from '../../../components/SearchStationModal/SearchStationModal';
 
+// image files
+import searchIcon from '../../../assets/StationList/searchIcon.svg';
+import createIcon from '../../../assets/StationList/createIcon.svg';
+import noStationLogo from '../../../assets/StationList/noStationLogo.png';
+
 const ShowStationList: React.FC = () => {
-  const [character, setCharacter] = useState<Character | null>(null);
+  //const [character, setCharacter] = useState<Character | null>(null);
   const [stations, setStations] = useState<Stations | null>(null);
   const [sortCriteria, setSortCriteria] = useState<"최신순" | "이름순">("최신순");
   const [isSearchStationModalVisible, setIsSearchStationModalVisible] = useState(false);
@@ -33,36 +35,24 @@ const ShowStationList: React.FC = () => {
   };
 
   useEffect(() => {
-    setCharacter(characterData);
+    //setCharacter(characterData);
     setStations(stationsData);
-    // fetch("/src/mock/character1.json")
-    //   .then((res) => res.json())
-    //   .then((data) => {setCharacter(data)})
-    //   .catch((err) => {console.error(err)});
-    // fetch("/src/mock/myStations.json")
-    //   .then((res) => res.json())
-    //   .then((data) => {setStations(data)})
-    //   .catch((err) => {console.error(err)});
+    //setStations({"stations": []});
   }, []);
 
-  const sortedStations = React.useMemo(() => {
-    if (!stations) return null;
+  const sortedByRecent = useMemo(() => (stations ? [...stations.stations] : []), [stations]);
 
-    const sorted = [...stations.stations];
-    if (sortCriteria === "최신순") {
-      sorted.sort((a, b) => b.createdAt - a.createdAt); // 타임스탬프 기준 내림차순 정렬
-    } else if (sortCriteria === "이름순") {
-      sorted.sort((a, b) => a.name.localeCompare(b.name, "ko")); // 이름순 오름차순 정렬
-    }
+  const sortedByName = useMemo(() => {
+    return stations ? [...stations.stations].sort((a, b) => a.name.localeCompare(b.name, 'ko')) : [];
+  }, [stations]);
 
-    return sorted;
-  }, [stations, sortCriteria]);
+  const sortedStations = sortCriteria === "최신순" ? sortedByRecent : sortedByName;
       
   return (
     <div className={s.container}>
       <div className={s.headContainer}>
         <div className={s.title}>
-          <div className={s.titleText}>{character?.userName}님 어디로 떠나볼까요?</div>
+          <div className={s.titleText}>Example님 어디로 떠나볼까요?</div>
           <div className={s.titleButtons}>
             <button
               className={s.searchStationButton}
@@ -82,7 +72,7 @@ const ShowStationList: React.FC = () => {
 
       <div className={s.separator}>
         <div className={s.totalNumOfStations}>
-          전체 {stations?.stations?.length || 0}
+          전체 {sortedStations.length || 0}
         </div>
         <div className={s.sortCriteriaBoxContainer}>
           <SortCriteriaBox
@@ -94,20 +84,24 @@ const ShowStationList: React.FC = () => {
       </div>
       <div className={s.stationListWrapper}>
         <div className={s.stationListContainer}>
-          {sortedStations?.map((station) => (
-            <div key={station.id} className={s.stationDisplayWrapper}>
-              <StationDisplay
-                name={station.name}
-                numOfUsers={station.numOfUsers}
-              />
+          {sortedStations.length > 0 ? (
+            sortedStations.map((station) => (
+              <div key={station.stationId} className={s.stationDisplayWrapper}>
+                <StationDisplay name={station.name} numOfUsers={station.numOfUsers} />
+              </div>
+            ))
+          ) : (
+            <div className={s.noStation}>
+              <span className={s.noStationTitle}>아직 등록된 정거장이 없어요...</span>
+              <span className={s.noStationText}>정거장을 검색하거나, 새로운 정거장을 만들어 보세요!</span>
+              <img className={s.noStationLogo} src={noStationLogo} alt='noStationLogo' />
             </div>
-            
-          ))}
+          )}
         </div>
       </div>
       {isSearchStationModalVisible && (
         <SearchStationModal
-          name={character?.userName}
+          name={"example"}
           handleSubmit={handleSubmit}
           searchValue={searchValue}
           setSearchValue={setSearchValue}
