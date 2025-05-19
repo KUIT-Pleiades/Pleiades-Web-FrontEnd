0fe46c53868fe3d002e0dba133c2f9ee53fb31fa
hyungyu-02 <yona20020209@gmail.com>
2025-05-19 18:42:58 +0900

    refactor: 검색 기록 삭제버튼 눌리는 영역 확장

--- src/pages/SearchUsers/RecentSearch/ShowRecentSearchUser/ShowRecentSearchUser.tsx
+++ src/pages/SearchUsers/RecentSearch/ShowRecentSearchUser/ShowRecentSearchUser.tsx
@@ -13,23 +13,28 @@ interface RecentSearchUserProps {
 
 const ShowRecentSearchUser: React.FC<RecentSearchUserProps> = ({ id, name, profileImage, onRemove, onClick }) => {
   return (
-    <div className={s.userContainer} onClick={() => onClick()}>
-      {/* 프로필 이미지 */}
-      <div className={s.profileImageContainer}>
-        <img src={profileImage} alt={`${name}'s profile`} className={s.profileImage} />
-      </div>
+    <div className={s.userContainer}>
+      <div className={s.profileSection} onClick={() => onClick()}>
+        {/* 프로필 이미지 */}
+        <div className={s.profileImageContainer}>
+          <img src={profileImage} alt={`${name}'s profile`} className={s.profileImage} />
+        </div>
 
-      {/* 사용자 정보 */}
-      <div className={s.userInfo}>
-        <p className={s.userName}>{name}</p>
-        <p className={s.userId}>@{id}</p>
+        {/* 사용자 정보 */}
+        <div className={s.userInfo}>
+          <p className={s.userName}>{name}</p>
+          <p className={s.userId}>@{id}</p>
+        </div>
       </div>
 
       {/* 삭제 버튼 */}
-      <button onClick={(e) => {
-        e.stopPropagation(); // 부모 클릭 이벤트 방지
-        onRemove(id);
-      }} className={s.removeButton}>
+      <button 
+        onClick={() => {
+          //e.stopPropagation(); // 부모 클릭 이벤트 방지
+          onRemove(id);
+        }}
+        className={s.removeButton}
+      >
         <img src={recentSearchUserRemove} alt='remove' />
       </button>
     </div>
