3a2cc9e018ced4bea6ecaa935ddf860e1378c710
hyungyu-02 <yona20020209@gmail.com>
2025-08-28 19:22:24 +0900

    feat: market home base setting

--- src/pages/Market/MarketHome.tsx
+++ src/pages/Market/MarketHome.tsx
@@ -1,28 +1,38 @@
+import s from './MarketHome.module.scss';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const MarketHome: React.FC = () => {
-  return (
-    <div>
-      <h1>마켓</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/market/official-store">공식-중고몰</Link>
-          </li>
-          <li>
-            <Link to="/market/my-item-sell">내 아이템 판매</Link>
-          </li>
-          <li>
-            <Link to="/market/my-product-management">내 상품 관리</Link>
-          </li>
-          <li>
-            <Link to="/market/transaction-history">거래 내역</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+    const navigate = useNavigate();
+
+    const buttons = [
+        { label: '공식/중고몰', path: '/market/official-store'},
+        { label: '내 아이템 판매', path: '/market/my-item-sell'},
+        { label: '내 상품 관리', path: '/market/my-product-management'},
+        { label: '거래 내역', path: '/market/transaction-history'},
+    ];
+
+    return (
+        <div className={s.container}>
+            <div className={s.header}>
+                <span className={s.headerTitle}>플레이아데스 상점</span>
+            </div>
+
+            <div className={s.content}>
+                <div className={s.gridContainer}>
+                    {buttons.map((btn, idx) => (
+                        <div
+                            key={idx}
+                            className={s.marketButton}
+                            onClick={() => navigate(btn.path)} // ✅ 클릭 시 이동
+                        >
+                            <div className={s.label}>{btn.label}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default MarketHome;
