82943e22c1a625acf74d1811827957443c4447da
hyungyu-02 <yona20020209@gmail.com>
2025-09-02 22:22:26 +0900

    feat: asset box upgrade

--- src/components/Asset/AssetBox.tsx
+++ src/components/Asset/AssetBox.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import s from './AssetBox.module.scss';
 
-import coin from "../../../assets/market/coin.svg";
-import stone from "../../../assets/market/stone.svg";
+import coin from "../../assets/market/coin.svg";
+import stone from "../../assets/market/stone.svg";
 
 interface AssetBoxProps {
     coinAmount: number;
@@ -12,12 +12,10 @@ interface AssetBoxProps {
 const AssetBox: React.FC<AssetBoxProps> = ({ coinAmount, stoneAmount }) => {
     return (
         <div className={s.itemAssets}>
-            <div className={s.asset}>
-                <img src={coin} alt="코인" />
-                <span>{coinAmount}</span>
-                <img src={stone} alt="돌맹이" />
-                <span>{stoneAmount}</span>
-            </div>
+            <img className={s.coinIcon} src={coin} alt="코인" />
+            <span className={s.coinAmount}>{coinAmount}</span>
+            <img className={s.stoneIcon} src={stone} alt="돌맹이" />
+            <span className={s.stoneAmount}>{stoneAmount}</span>
         </div>
     );
 };
