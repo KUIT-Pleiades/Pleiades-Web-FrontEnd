8fb9f228d1c5faf1cdfec4dbbff99e578ad4c4bf
hyungyu-02 <yona20020209@gmail.com>
2025-09-26 14:52:34 +0900

    refactor(seo): Apply AdSense meta tag dynamically on landing page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { HelmetProvider } from 'react-helmet-async';
 
 // 1. React Query 관련 import
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -14,7 +15,9 @@ createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     {/* 3. QueryClientProvider로 App 전체를 감싸줌 */}
     <QueryClientProvider client={queryClient}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
