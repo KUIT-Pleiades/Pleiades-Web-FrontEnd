e01b832ca2da4d5f8e98aa5263740a06615c29f6
FrogUni <80472206+danpung628@users.noreply.github.com>
2025-10-02 13:08:30 +0900

    cloth -> fashion 변경

--- src/api/marketApi.ts
+++ src/api/marketApi.ts
@@ -24,7 +24,7 @@ export const getOfficialFaceItems = async (): Promise<OfficialFaceData> => {
  */
 export const getOfficialClothItems = async (): Promise<OfficialClothData> => {
   const response = await axiosRequest<OfficialClothData>(
-    "/store/official/cloth",
+    "/store/official/fashion",
     "GET",
     null
   );
