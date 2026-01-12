6fd9a74548181fa5a2a2de23217167e55f0a6121
FrogUni <80472206+danpung628@users.noreply.github.com>
2026-01-13 02:03:20 +0900

    친구별 및 정거장 이미지 경로 변경

--- src/functions/getImage.ts
+++ src/functions/getImage.ts
@@ -1,5 +1,5 @@
 export function getImage(itemName: string) {
   const PINATA_ENDPOINT = import.meta.env.VITE_IMG_BASE_URL;
-  const requestPoint = `${PINATA_ENDPOINT}${itemName}`;
+  const requestPoint = `${PINATA_ENDPOINT}background/${itemName}`;
   return requestPoint;
 }
